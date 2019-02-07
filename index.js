05a0673740e41ef751ca9df1572b84b9d5dc5ac6
TungNguyen <nguyenxuantung1221@gmail.com>
2019-02-07 23:08:24 +0700

    dog eat cat

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 var Dog = require('./Dog.js');
+var Cat = require('./Cat.js');
+
 
 var dogTom = new Dog('Tom');
 
-dogTom.sayHi();
+var meoTom = new Cat('Tom');
+
+dogTom.eat(meoTom);
+
+console.log(dogTom);
